05b00a3cb54e9b73e633cf2158eb67d06e733408
Joy <joyyujiepeng@gmail.com>
2024-01-17 09:05:28 +0000

    feat: add dynamic web page tag title for different organization page

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,14 @@
 import { OrgControl } from "./_components/OrgControl"
+import { auth } from "@clerk/nextjs"
+import { startCase } from "lodash"
+
+// generateMetadata (correct) generateMetaData(wrong)
+export function generateMetadata() {
+  const { orgSlug } = auth()
+  return {
+    title: startCase(orgSlug || "organization"),
+  }
+}
 
 export default function OrganizationIdLayout({
   children,
