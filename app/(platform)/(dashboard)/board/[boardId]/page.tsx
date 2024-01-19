d0f1b390b9e403c61d61956b8c23b62ae04e7cb5
Joy <joyyujiepeng@gmail.com>
2024-01-19 22:16:25 +0000

    feat: add boardId page and fetch lists data

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,5 +1,35 @@
-const BoardIdPage = () => {
-  return <div>BoardIdPage</div>
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+import { db } from "@/lib/db"
+
+interface BoardIdPageParam {
+  params: {
+    boardId: string
+  }
+}
+
+const BoardIdPage = async ({ params }: BoardIdPageParam) => {
+  const { orgId } = auth()
+  if (!orgId) {
+    redirect("/select-org")
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+  })
+  return <div></div>
 }
 
 export default BoardIdPage
